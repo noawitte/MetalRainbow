e066519a9f2d44aebd762288ffb5dde1471201e7
Noa Witte <noawitte@icloud.com>
2023-09-23 17:33:13 +0200

    Initial Commit

---
+++ MetalSwiftUI/MetalSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalSwiftUIApp.swift
+//  MetalSwiftUI
+//
+//  Created by Noa Witte on 23/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MetalSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
